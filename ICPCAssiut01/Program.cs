5bf1079765680f85542166adf1f8e082859e91df
entsarahmed <entesara500@gmail.com>
2025-02-11 00:04:58 +0200

    B/ Only one line containing the following space-separated values: int, long long, char, float and double respectively.

--- ICPCAssiut01/Program.cs
+++ ICPCAssiut01/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ICPCAssiut01
+﻿using System.Reflection.Metadata.Ecma335;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ICPCAssiut01
 {
     internal class Program
     {
@@ -6,15 +9,66 @@
         {
             #region A/ Given a name S. Print "Hello, (name)" without parentheses. Input Only one line containing a string S.  OutputPrint "Hello, " without quotes, then print name.
 
-            Console.WriteLine("Enter Your Name");
-            string S = Console.ReadLine()!;
+            /* Console.WriteLine("Enter Your Name");
+             string S = Console.ReadLine()!;
 
-            string Name = S.Insert(0, "Hello, ");
+             string Name = S.Insert(0, "Hello, ");
 
-            Console.WriteLine(Name);
+             Console.WriteLine(Name);*/
 
             #endregion
 
+
+            #region B/ Only one line containing the following space-separated values: int, long long, char, float and double respectively.
+
+
+
+            //Console.WriteLine("Enter Space-Separated values: int, long, char, float, double");
+
+            //string[] input = Console.ReadLine().Split(' ');
+
+            //int IntNumber;
+            //bool Flag;
+            //long LongNumber;
+            //char charact;
+            //float FloatNumber;
+            //double DoubleNumber;
+
+
+            //do
+            //{
+            //    Flag = int.TryParse(input[0], out IntNumber);
+            //    Console.WriteLine(IntNumber);
+            //}
+            //while (!Flag);
+            //do
+            //{
+            //    Flag = long.TryParse(input[1], out LongNumber);
+            //    Console.WriteLine(LongNumber);
+            //}while (!Flag);
+            //do
+            //{
+            //    Flag = char.TryParse(input[2], out  charact);
+            //    Console.WriteLine(charact);
+            //}while (!Flag);
+
+            //do
+            //{
+            //    Flag = float.TryParse(input[3],out FloatNumber);
+            //    Console.WriteLine(FloatNumber);
+            //}while(!Flag);
+
+            //do
+            //{
+            //    Flag = double.TryParse(input[4], out DoubleNumber);
+            //    DoubleNumber=Math.Round(DoubleNumber,2);
+            //    Console.WriteLine(DoubleNumber);
+            //} while (!Flag);
+
+
+
+
+            #endregion
         }
 
     }
