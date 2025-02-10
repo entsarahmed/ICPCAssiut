4a041b72d78b21c2187f2ec09dae8301a07ffb61
entsarahmed <entesara500@gmail.com>
2025-02-10 20:17:37 +0200

    Given a name S. Print "Hello, (name)" without parentheses.

--- ICPCAssiut01/Program.cs
+++ ICPCAssiut01/Program.cs
@@ -6,12 +6,12 @@
         {
             #region A/ Given a name S. Print "Hello, (name)" without parentheses. Input Only one line containing a string S.  OutputPrint "Hello, " without quotes, then print name.
 
-            //Console.WriteLine("Enter Your Name");
-            //string S = Console.ReadLine()!;
+            Console.WriteLine("Enter Your Name");
+            string S = Console.ReadLine()!;
 
-            //string Name = S.Insert(0, "Hello, ");
+            string Name = S.Insert(0, "Hello, ");
 
-            //Console.WriteLine(Name);
+            Console.WriteLine(Name);
 
             #endregion
 
