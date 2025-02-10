e398625d5f82ecba82680bf8f461d80257725b8b
entsarahmed <entesara500@gmail.com>
2025-02-10 20:16:34 +0200

    Add project files.

---
+++ ICPCAssiut01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ICPCAssiut01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region A/ Given a name S. Print "Hello, (name)" without parentheses. Input Only one line containing a string S.  OutputPrint "Hello, " without quotes, then print name.
+
+            //Console.WriteLine("Enter Your Name");
+            //string S = Console.ReadLine()!;
+
+            //string Name = S.Insert(0, "Hello, ");
+
+            //Console.WriteLine(Name);
+
+            #endregion
+
+        }
+
+    }
+}
